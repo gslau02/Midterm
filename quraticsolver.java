4406f0eee2331304b7008e0ad420915a8079b0bf
Ge Siong Lau <gslau@umich.edu>
2022-10-21 04:37:02 +0800

    GeSiongLau changed integers

---
+++ quraticsolver.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class quraticsolver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 32;
+		int c = 22;
+		if (b*b - 4*a*c <0)
+			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
+		else
+			{
+				System.out.println("The solution for the equation " + a + "x^2 + " + b +"x + " + c +" are");
+				System.out.println((-b+Math.sqrt(b*b-4*a*c))/2*a + " and " + (-b-Math.sqrt(b*b-4*a*c))/2*a);
+			}
+	}
+
+}
